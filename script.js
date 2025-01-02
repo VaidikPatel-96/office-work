e084086ec5c088c5c66a090acd203f8ddd5bf370
vaidik96 <vaidikplt9@gmail.com>
2025-01-02 17:25:15 +0530

    function

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-document.writeln("hello"); 
+//document.writeln("hello"); 
+
+//console.log("javascript Tutorial");
+
+//var fristnsme="grestdtsck";
+
+// let x =10;
+
+// if(x>5){
+//     let y=20;
+//     console.log(y);
+// }
+ 
+// const a =4;
+// console.log(a);
+
+//#Golbal scope
+
+var x = "hello, GreatStack Golbal";
+function example(){
+    
+console.log(x);
+}
+example(); 
+
+// console.log(x);
+
+//##Function scope
+
+function example2(){
+    var fs="hello, GreatStack Function";
+}
+example2
+ console.log(fs);  //fs is not defined
+
+
+ //##Block scope
+
+  
