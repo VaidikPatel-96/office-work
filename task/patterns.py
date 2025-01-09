662e67fec2d4a810ba65e8e2b2de2743d2dc63d8
vaidik96 <vaidikplt9@gmail.com>
2025-01-09 11:59:37 +0530

    pattern

--- task/patterns.py
+++ task/patterns.py
@@ -72,3 +72,12 @@ for i in range(n,0,-1):
 # 3 2 1
 # 2 1
 # 1    
+
+# Q6
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ", end=" ")
+    for k in range(1,(2*i-1)+1):
+        print("*", end=" ")    
+    print("\n")
