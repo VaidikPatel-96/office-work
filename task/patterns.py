8ff87ecb7d6bbe60adb73444453169532a3dbca4
vaidik96 <vaidikplt9@gmail.com>
2025-01-09 11:54:29 +0530

    patterns

---
+++ task/patterns.py
@@ -0,0 +1,74 @@
+#Q1
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()    
+
+# output = 
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+#Q2
+r = 5
+for i in range(1,r+1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+# # output =
+# * 
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+#Q3
+
+n = 5
+for i in range(n, 0, -1):
+    for j in range(0, i):
+        print("*", end=" ")
+    print()
+
+# output =
+# * * * * * 
+# * * * *
+# * * *
+# * *
+# *
+
+
+# Q4    
+
+n= 5
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end=" ")
+    print()
+
+# output =
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# Q5
+
+n= 6
+
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j, end=" ")
+    print()
+
+# output =
+# 6 5 4 3 2 1
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1    
